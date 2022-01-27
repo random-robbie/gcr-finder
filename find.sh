bc8edd31f27acfe9ae3266e18a410f7f37a1bca1
Robert Wiggins <rob@what-security.co.uk>
2022-01-27 12:04:28 +0000

    Working

---
+++ find.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+check_target () {
+	if $(curl -i -s -k --proxy http://127.0.0.1:8085 -X 'POST' -H 'Content-Type: application/json' --data-binary "['$1',null,null,[11]]" https://console.cloud.google.com/m/gcr/entities/list | grep -qi "gcr.ListEntities"); then
+		echo "https://gcr.io/$1"
+	fi
+}
+export -f check_target
+>/tmp/targets.tmp
+# Scrape targets to get more targets
+echo "Generating List"
+rm -rf /tmp/targets.txt
+for line in `cat list.txt`; do
+echo "$1-$line" >> /tmp/targets.txt
+done
+wait
+echo "Testing targets..."
+parallel -a /tmp/targets.txt -j 25 --bar check_target | tee found.txt
+if [[ $(cat found.txt) == "" ]]; then
+	echo "No results found."
+else
+	echo "Done."
+fi
